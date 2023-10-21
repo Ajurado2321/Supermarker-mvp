3c94218338333d068909df9689f22fb02d20eac9
Ajurado2321 <129556540+Ajurado2321@users.noreply.github.com>
2023-10-21 04:20:01 -0500

    ViewProducto

--- Views/ProductView.cs
+++ Views/ProductView.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace supermarket.Views
 {
     public partial class ProductView : Form, IProductModelView
@@ -16,14 +15,13 @@ namespace supermarket.Views
         private bool isEdit;
         private bool isSuccessful;
         private string message;
-
         public ProductView()
         {
             InitializeComponent();
             AssociateAndRaiseViewEvents();
-
             btnClose.Click += delegate { this.Close(); };
         }
+
 
 
 
@@ -42,55 +40,46 @@ namespace supermarket.Views
             get { return message; }
             set { message = value; }
         }
-
         public string ProductModelId
         {
             get { return tabControl1.Text; }
             set { tabControl1.Text = value; }
         }
-
         public string ProductModeName
         {
             get { return TxtPayModeName.Text; }
             set { TxtPayModeName.Text = value; }
         }
-
         public string ProductModePrice
         {
             get { return TxtPayModeObservation.Text; }
             set { TxtPayModeObservation.Text = value; }
         }
-
         public string ProductModeStock
         {
             get { return TxtPayModeObservation.Text; }
             set { TxtPayModeObservation.Text = value; }
         }
-
         public string ProductModeCategoryId
         {
             get { return TxtPayModeObservation.Text; }
             set { TxtPayModeObservation.Text = value; }
         }
-
         public string SearchValue
         {
             get { return TxtSearch.Text; }
             set { TxtSearch.Text = value; }
         }
-
         public event EventHandler SearchEvent;
         public event EventHandler AddNewEvent;
         public event EventHandler EditEvent;
         public event EventHandler DeleteEvent;
         public event EventHandler SaveEvent;
         public event EventHandler CancelEvent;
-
         public void SetProductModeListBildingSource(BindingSource payModeList)
         {
             throw new NotImplementedException();
         }
-
         private void AssociateAndRaiseViewEvents()
         {
             btnSearch.Click += delegate { SearchEvent?.Invoke(this, EventArgs.Empty); };
@@ -102,28 +91,23 @@ namespace supermarket.Views
             btnsave.Click += delegate
             {
                 AddNewEvent?.Invoke(this, EventArgs.Empty);
-
                 tabControl1.TabPages.Remove(tabPageProductList);
                 tabControl1.TabPages.Add(tabPageProductDetail);
                 tabPageProductDetail.Text = "Add new Product";
             };
-
             btnEdit.Click += delegate
             {
                 EditEvent?.Invoke(this, EventArgs.Empty);
-
                 tabControl1.TabPages.Remove(tabPageProductList);
                 tabControl1.TabPages.Add(tabPageProductDetail);
                 tabPageProductDetail.Text = "Edit Product Mode";
             };
             btnDelet.Click += delegate
             {
-
                 var result = MessageBox.Show(
                     "Are sure you want to delete the selected Pay Mode",
                     "Warning",
                 MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-
                 if (result == DialogResult.Yes)
                 {
                     DeleteEvent?.Invoke(this, EventArgs.Empty);
@@ -133,7 +117,6 @@ namespace supermarket.Views
             btnsave.Click += delegate
             {
                 SaveEvent?.Invoke(this, EventArgs.Empty);
-
                 if (isSuccessful)
                 {
                     tabControl1.TabPages.Remove(tabPageProductDetail);
@@ -144,32 +127,25 @@ namespace supermarket.Views
             btnCancel.Click += delegate
             {
                 CancelEvent?.Invoke(this, EventArgs.Empty);
-
                 tabControl1.TabPages.Remove(tabPageProductDetail);
                 tabControl1.TabPages.Add(tabPageProductList);
             };
         }
-
         private void BtnSearch_Click(object? sender, EventArgs e)
         {
             throw new NotImplementedException();
         }
-
         public void SetPayModeListBildingSource(BindingSource payModeList)
         {
             DgProduct.DataSource = payModeList;
         }
-
         private static PayModeView instance;
-
-
         public static PayModeView GetInstance(Form parentContainer)
         {
             if (instance == null || instance.IsDisposed)
             {
                 instance = new PayModeView();
                 instance.MdiParent = parentContainer;
-
                 instance.FormBorderStyle = FormBorderStyle.None;
                 instance.Dock = DockStyle.Fill;
             }
