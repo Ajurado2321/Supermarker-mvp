ac3c76450ba659e7ce3bc250d872a07bc60c095b
Ajurado2321 <129556540+Ajurado2321@users.noreply.github.com>
2023-10-21 03:08:15 -0500

    PayMode

--- Program.cs
+++ Program.cs
@@ -1,17 +1,14 @@
-namespace Supermarker_mvp
+using supermarket.Views;
+
+namespace supermarket
 {
     internal static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
+@@ -11,7 +13,7 @@ static void Main()
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            Application.Run(new PayModeView());
         }
     }
 }
